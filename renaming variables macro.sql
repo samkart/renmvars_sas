4d3ed0294407fee19b0d25e028351803f23d15fb
Shubhradeep Majumdar <smaj125@gmail.com>
2018-01-22 19:10:16 +0530

    Add files via upload
    The SAS macro code goes through all the variables of a table and gives a macro var of variables after prefixing a character of your choice. Use it in your data step rename function.

---
+++ renaming variables macro.sql
@@ -0,0 +1,11 @@
+options mprint;
+
+%macro prefixvar(lib/*libname*/,tab/*table*/,char/*character to prefix*/,macronm/*name of the macro to create*/);
+%global m&macronm.;
+proc sql;
+select cats(name,"=","&char.",name) into:m&macronm. separated by " "
+from dictionary.columns
+where libname="%upcase(&lib.)" and memname="%upcase(&tab.)" and upcase(name) not like "%QTR%";
+quit;
+%put |&&m&macronm.|;
+%mend prefixvar;
