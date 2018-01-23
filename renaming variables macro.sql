b58a85804787d9da0bb3096f02b38f524a0d3dd6
Shubhradeep Majumdar <smaj125@gmail.com>
2018-01-23 16:10:29 +0530

    Generalized it

--- renaming variables macro.sql
+++ renaming variables macro.sql
@@ -3,7 +3,7 @@ options mprint;
 %macro prefixvar(lib/*libname*/,tab/*table*/,char/*character to prefix*/,macronm/*name of the macro to create*/);
 %global m&macronm.;
 proc sql;
-select cats(name,"=","&char.",name) into:m&macronm. separated by " "
+select cats('"',name,'"n="',"&&char.",name,'"n') into:m&macronm. separated by " "
 from dictionary.columns
 where libname="%upcase(&lib.)" and memname="%upcase(&tab.)" and upcase(name) not like "%QTR%";
 quit;
